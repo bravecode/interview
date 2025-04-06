cc6443089eb5edaf242bf592995e0ed9e9f8debd
Bravecode <contact@bravecode.eu>
2025-04-06 04:56:51 +0200

    feat:increased stale time for queries to 5 minutes

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 type QueryProviderProps = {
   children: React.ReactNode;
